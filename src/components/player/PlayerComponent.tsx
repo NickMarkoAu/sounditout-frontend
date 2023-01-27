5d5c7e31ccdcdc71ccbe61528766981a7ed97b57
Nick Marko <nick.marko@prospection.com.au>
2023-01-27 22:54:56 +1100

    Initial commit

---
+++ src/components/player/PlayerComponent.tsx
@@ -0,0 +1,13 @@
+import { registerRootComponent } from 'expo';
+import TrackPlayer from 'react-native-track-player';
+import App from './App';
+
+
+registerRootComponent(App);
+TrackPlayer.registerPlaybackService(() => require('./service'));
+
+const PlayerComponent = () => {
+
+}
+
+export default PlayerComponent;
---
+++ src/state/song-suggestion.model.tsx
@@ -0,0 +1,9 @@
+export interface Song {
+  id: number;
+  userId: string;
+  imageId: string;
+  name: string;
+  artist: string;
+  tags: string[];
+  youtubeVideoId: string;
+}
